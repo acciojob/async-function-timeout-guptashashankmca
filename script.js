e356bec5b9da2c6998b6d4dafde0c20898813fe0
guptashashankmca <165798455+guptashashankmca@users.noreply.github.com>
2024-09-03 15:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function() {
+    const text = document.getElementById('text').value;
+    const delay = parseInt(document.getElementById('delay').value);
+
+    await delayMessage(text, delay);
+});
+
+async function delayMessage(message, delay) {
+    await new Promise(resolve => setTimeout(resolve, delay));
+    document.getElementById('output').textContent = message;
+}
